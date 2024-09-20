0ce5952fe5282ee3aebde5dca3c9f7278281b08a
RRRRReus <neo@tianxuanji>
2024-09-21 00:07:53 +0800

    arm

--- lab1/arm/Fibonacci.c
+++ lab1/arm/Fibonacci.c
@@ -1,25 +1,32 @@
-#include <stdio.h>
+#include<stdio.h>
+
+int Fib(int n)
+{
+    if(n == 0)
+    {
+        return 0;
+    }
+    else if(n == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return Fib(n - 1) + Fib(n - 2);
+    }
+
+}
 
 int main()
 {
-    int a, b, i, t, n;
-    a = 0;
-    b = 1;
-    i = 1;
-
+    int n;
+    printf("需要计算斐波那契数列的第几位:");
     scanf("%d", &n);
-
-    printf("%d\n", a);
-    printf("%d\n", b);
-    
-    while (i < n)
+    if(n < 0)
     {
-        t = b;
-        b = a + b;
-        printf("%d\n", b);
-        a = t;
-        i = i + 1;
+        printf("输入错误\n");
+        return 0;
     }
-
+    printf("第%d项为:%d\n", n,Fib(n));
     return 0;
 }
--- lab1/arm/auto.sh
+++ lab1/arm/auto.sh
@@ -2,7 +2,7 @@
 
 # Step 1: Compile the ARM assembly file
 echo "Compiling Fibonacci.S..."
-arm-linux-gnueabihf-gcc Fibonacci.S -o Fibonacci.out
+arm-linux-gnueabihf-gcc Fibonacci.S -o Fibonacci.out  #告诉编译器不要使用标准的启动文件和库 -nostartfiles 和 -nostdlib
 
 # Check if compilation was successful
 if [ $? -ne 0 ]; then
