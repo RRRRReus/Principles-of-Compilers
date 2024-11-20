534baa61bf53df545eea88980ed35e3db54fee74
2212323 <2212323@mail.nankai.edu.cn>
2024-11-20 17:09:47 +0800

    # 脚本修改
    增加了链接库文件的脚本，并去掉了clang的C++名字修饰

--- compare.sh
+++ compare.sh
@@ -4,7 +4,7 @@ make run
 
 echo "生成自己的中间代码结束，开始生成clang的中间代码"
 
-clang -target arm-linux-gnueabihf -mcpu=cortex-a8 -mfloat-abi=hard -x c++ -S -emit-llvm example.sy -o exampleOK.ll
+clang -target arm-linux-gnueabihf -mcpu=cortex-a8 -mfloat-abi=hard -x c -S -emit-llvm example.sy -o exampleOK.ll
 
 echo "自己的运行结果"
 
--- run.sh
+++ run.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-# 确保传入了参数
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <file.ll>"
-    exit 1
-fi
-
 # 定义变量
 INPUT_FILE="$1"
 OUTPUT_FILE="output"
 EXECUTABLE="hello244"
+SYSLIB_PATH="sysyruntimelibrary"  # 运行时库路径
 
 # 清理上次生成的文件
 rm -f "$OUTPUT_FILE" "$EXECUTABLE"
 
-# 生成目标文件
-clang --target=arm-linux-gnueabihf -mcpu=cortex-a8 -mfloat-abi=hard -o "$EXECUTABLE" "$INPUT_FILE" -lm
+# 检查输入文件是否存在
+if [ ! -f "$INPUT_FILE" ]; then
+    echo "Error: Input file does not exist."
+    exit 1
+fi
+
+# 生成目标文件并链接运行时库
+clang --target=arm-linux-gnueabihf -mcpu=cortex-a8 -mfloat-abi=hard -fcommon -I"$SYSLIB_PATH" -o "$EXECUTABLE" "$INPUT_FILE" "$SYSLIB_PATH/sylib.c" -lm
 
 # 检查是否成功生成了可执行文件
 if [ ! -f "$EXECUTABLE" ]; then
@@ -29,12 +30,11 @@ export LD_LIBRARY_PATH=/usr/arm-linux-gnueabihf/lib
 # 运行 QEMU
 qemu-arm -L /usr/arm-linux-gnueabihf/ ./"$EXECUTABLE"
 
-
 # 获取返回值
 retval=$?
 
 # 输出返回值
 echo "Program exited with return value: $retval"
- 
+
 # 清理生成的文件
 rm -f "$EXECUTABLE"
